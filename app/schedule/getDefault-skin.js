cfa94f2f2ac4b00d2b663fd55f4e663c72b9705a
huangjin <huangjin@chainup.com>
2020-07-27 10:17:29 +0800

    CHAINUP-11604

--- app/schedule/getDefault-skin.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultSkin, getDefaultSkinConfig } = require('BlockChain-ui/node/schedule/getDefault-skin');
-
-module.exports = {
-  schedule: getDefaultSkinConfig,
-  async task(ctx) {
-    getDefaultSkin(this.schedule, ctx.logger, ctx);
-  },
-};
