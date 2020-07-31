7ead0728fb15c385f72c15fac9e50c8d9b0b4b05
zhuwentao <zhuwt888@sina.com>
2020-07-31 15:04:49 +0800

    IEO 重构首页ipfs列表

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ module.exports = (app) => {
   const { router, controller } = app;
   if (app.config.env === 'local'){
     router.post('/fe-ex-api/*', controller.devProxy.index);
+    router.post('/fe-hashrate-api/*', controller.devProxy.index);
   }
   router.get('/home/getMarket/:id', controller.getMarket.index);
   router.get('/(.+)?/', controller.homePage.index);
