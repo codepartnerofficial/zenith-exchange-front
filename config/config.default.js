48b6f56105da563d130cfc2744a3e775ec9621a3
renzeyang <renzeyang@chainup.com>
2021-01-26 16:48:16 +0800

    切换

--- config/config.default.js
+++ config/config.default.js
@@ -59,7 +59,7 @@ module.exports = (appInfo) => {
     // myAppName: 'egg',
   };
   config.devUrlProxy = {
-     ex: 'https://www.momoex.vip',
+     ex: 'https://www.bitwind.com',
     //ex:'https://www.vbcoin.ooo',
     otc: 'https://www.mkex.me',
     futures: 'https://futures.bbkx.com',
