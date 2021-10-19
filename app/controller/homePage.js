51f421f9a6a9f20a321630e10e0ece5a17f5b403
wangyi-nicole <m15210181461@163.com>
2021-10-19 17:55:02 +0800

    修改

--- app/controller/homePage.js
+++ app/controller/homePage.js
@@ -236,8 +236,8 @@ class StaticIndex extends Controller {
 
   getTemplate(ispc) {
     let template = 'international';
-    const indexTempType = this.publicInfo.switch.index_temp_type;
-    // const indexTempType = '51';
+    // const indexTempType = this.publicInfo.switch.index_temp_type;
+    const indexTempType = '53';
     if (this.publicInfo.switch && templateConfig[indexTempType]) {
       template = templateConfig[indexTempType];
     }
